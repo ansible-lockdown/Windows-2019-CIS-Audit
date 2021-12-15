9026d12f3028e05d3b513e626a4e91948ca4dcf4
Mark Bolwell <mark.bollyuk@gmail.com>
2021-12-15 12:04:56 +0000

    updated
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- run_audit.ps1
+++ run_audit.ps1
@@ -140,7 +140,7 @@ $audit_content="$AUDIT_CONTENT_DIR"
 # Has to be a better way
 
 
-$AUDIT_JSON_VARS = "{ 'machine_uuid': `'$machine_uuid`','os_deployment_type': `'$system_type`', 'epoch': `'$epoch`', 'audit_run': `'$audit_run`', 'os_locale': `'$os_locale`', 'os_release': `'$os_version`', 'windows2019cis_os_distribution': `'$os_name`', 'os_hostname': `'$os_hostname`', 'auto_group': `'$auto_group`', 'gpresult_file': `'$$gpresult_file`', 'auditresult_file': `'$auditresult_file`', 'secedit_file': `'$secedit_file`','audit_content': `'$audit_content`'}"
+$AUDIT_JSON_VARS = "{ 'benchmark':`'$BENCHMARK`','machine_uuid': `'$machine_uuid`','epoch': `'$epoch`', 'os_deployment_type': `'$system_type`',  'os_locale': `'$os_locale`', 'os_release': `'$os_version`', 'os_distribution': `'$os_name`', 'os_hostname': `'$os_hostname`', 'auto_group': `'$auto_group`', 'gpresult_file': `'$$gpresult_file`', 'auditresult_file': `'$auditresult_file`', 'secedit_file': `'$secedit_file`','audit_content': `'$audit_content`'}"
 
 
 # Set up AUDIT_OUT
